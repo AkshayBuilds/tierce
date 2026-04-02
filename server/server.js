f3a9cb075ef9124680c95ab2ead0ca48605d7992
Akshay ChaudharY <akshaychaudhary@Akshays-MacBook-Air.local>
2026-04-02 16:57:48 +0530

    cors

--- server/server.js
+++ server/server.js
@@ -21,7 +21,10 @@ app.use(express.json())
 
 app.use(cors({
   origin: [
-    "https://forever-admin-gray-delta.vercel.app"
+    "https://forever-admin-gray-delta.vercel.app",
+    "https://forever-kappa-seven.vercel.app",
+    "http://localhost:5173",
+    "http://localhost:5173"
   ],
   credentials: true,
   methods: "GET,POST,PUT,DELETE,OPTIONS",
