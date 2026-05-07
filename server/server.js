28256cc1aafdb86a3b800a02fff5fae8dbd57f85
Akshay Chaudhary <chaudharyakshay307@gmail.com>
2026-05-07 15:59:10 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ app.use(express.json())
 
 app.use(cors({
   origin: [
-    "https://tierce-ecom.vercel.app/"
+    "https://tierce-ecom.vercel.app"
   ],
   credentials: true,
   methods: "GET,POST,PUT,DELETE,OPTIONS",
