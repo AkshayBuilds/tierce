f4b8ce0ca017da9ae914a17c973c1f86314e04c2
Akshay Chaudhary <chaudharyakshay307@gmail.com>
2026-05-07 15:49:57 +0530

    Cors Url changes

--- server/server.js
+++ server/server.js
@@ -20,8 +20,7 @@ app.use(express.json())
 
 app.use(cors({
   origin: [
-    "http://localhost:5173",
-    "http://localhost:5174",
+    "https://tierce-ecom.vercel.app"
   ],
   credentials: true,
   methods: "GET,POST,PUT,DELETE,OPTIONS",
